b3b540c9eb1a1d34959c4ade12868c569deb91b1
anandb104 <210169445+anandb104@users.noreply.github.com>
2026-04-08 00:08:21 +0530

    Add logic to show x and o on the grid Logic to insert the marker on the grid was added by the use of id and making its text content as the marker.

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ let game=(function(){
     let player1=player("random","X");
     let player2=player("computer","O");
     let currentplayer=player1;
+    function Currentplayer(){
+        return currentplayer;
+    }
     function switchplayer(){
       if(currentplayer==player1){
         currentplayer=player2;
@@ -108,16 +111,60 @@ let game=(function(){
     console.log(`${currentplayer.name} has won already!! Reset to Start a new game.`);
 }
     }
-    return{play};
+    return{play,Currentplayer};
 })();
+
+function dom(){
+    let gamer=game;
+    let index0=document.getElementById("index-0");
+    let index1=document.getElementById("index-1");
+    let index2=document.getElementById("index-2");
+    let index3=document.getElementById("index-3");
+    let index4=document.getElementById("index-4");
+    let index5=document.getElementById("index-5");
+    let index6=document.getElementById("index-6");
+    let index7=document.getElementById("index-7");
+    let index8=document.getElementById("index-8");
+    function setdom(index){
+        if(index==0)
+        index0.textContent=`${gamer.Currentplayer().marker}`;
+        if(index==1)
+        index1.textContent=`${gamer.Currentplayer().marker}`
+        if(index==2)
+        index2.textContent=`${gamer.Currentplayer().marker}`
+        if(index==3)
+        index3.textContent=`${gamer.Currentplayer().marker}`
+        if(index==4)
+        index4.textContent=`${gamer.Currentplayer().marker}`
+        if(index==5)
+        index5.textContent=`${gamer.Currentplayer().marker}`
+        if(index==6)
+        index6.textContent=`${gamer.Currentplayer().marker}`
+        if(index==7)
+        index7.textContent=`${gamer.Currentplayer().marker}`
+        if(index==8)
+        index8.textContent=`${gamer.Currentplayer().marker}`
+    }
+    return {setdom};
+}
+let display=dom();
 game.play(0);
+display.setdom(0);
 game.play(2);
+display.setdom(2);
 game.play(6);
+display.setdom(6);
 game.play(3);
+display.setdom(3);
 game.play(8);
+display.setdom(8);
 game.play(4);
+display.setdom(4);
 game.play(5);
+display.setdom(5);
 game.play(7);
+display.setdom(7);
 game.play(1);
+display.setdom(1);
 game.play(1);
 
